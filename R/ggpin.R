09a3c80b45af08a0c1e6e39914798cb3a383b9c8
Martin Zuba <martin.zuba@gmx.net>
2019-09-30 09:42:41 +0200

    revert size/height issue

--- R/ggpin.R
+++ R/ggpin.R
@@ -40,7 +40,7 @@ draw_key_pin <- function(data, params, size) {
 
 GeomPin <- ggproto("GeomPin", Geom,
                    required_aes = c("x", "y"),
-                   default_aes = c(colour = "darkred", size = 1),
+                   default_aes = c(colour = "darkred", width = 1/50),
                    draw_key = draw_key_pin,
 
                    draw_panel = function(data, panel_params, coord) {
@@ -51,7 +51,7 @@ GeomPin <- ggproto("GeomPin", Geom,
                      out <- grid::rasterGrob(image = mz_colourpin(farbe),
                                              x = coords[coords$colour == farbe,]$x,
                                              y = coords[coords$colour == farbe,]$y,
-                                             width = coords$size / 50,
+                                             width = coords$width,
                                              just = c("centre", "bottom"))
 
                      if (length(unique(coords$colour)) > 1)
@@ -60,7 +60,7 @@ GeomPin <- ggproto("GeomPin", Geom,
                                             grid::rasterGrob(image = mz_colourpin(f),
                                                              x = coords[coords$colour == f,]$x,
                                                              y = coords[coords$colour == f,]$y,
-                                                             width = coords$size / 50,
+                                                             width = coords$width,
                                                              just = c("centre", "bottom")))
                        }
 
@@ -89,36 +89,6 @@ GeomPin <- ggproto("GeomPin", Geom,
 geom_pin <- function(mapping = NULL, data = NULL, stat = "identity",
                      position = "identity", na.rm = FALSE, show.legend = NA,
                      inherit.aes = TRUE, ...) {
-
-  GeomPin <- ggproto("GeomPin", Geom,
-                     required_aes = c("x", "y"),
-                     default_aes = c(colour = "darkred", size = 1),
-                     draw_key = draw_key_pin,
-
-                     draw_panel = function(data, panel_params, coord) {
-                       coords = coord$transform(data, panel_params)
-                       coords = coords[order(-coords$y - coords$x),]
-
-                       farbe <- unique(coords$colour)[1]
-                       out <- grid::rasterGrob(image = mz_colourpin(farbe),
-                                               x = coords[coords$colour == farbe,]$x,
-                                               y = coords[coords$colour == farbe,]$y,
-                                               width = coords$width,
-                                               just = c("centre", "bottom"))
-
-                       if (length(unique(coords$colour)) > 1)
-                         for(f in unique(coords$colour)[-1]) {
-                           out <- grid::gList(out,
-                                              grid::rasterGrob(image = mz_colourpin(f),
-                                                               x = coords[coords$colour == f,]$x,
-                                                               y = coords[coords$colour == f,]$y,
-                                                               width = coords$width,
-                                                               just = c("centre", "bottom")))
-                         }
-
-                       return(out)
-
-                     })
 
   layer(
     geom = GeomPin, mapping = mapping, data = data, stat = stat,
